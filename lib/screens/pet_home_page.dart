efe1b3903edc537249ba9ddfe2ba5a4e9451cc5b
ragulnathan <ragulsarma@gmail.com>
2024-03-11 23:00:13 +0530

    pet home page alignment fix

--- lib/screens/pet_home_page.dart
+++ lib/screens/pet_home_page.dart
@@ -14,72 +14,63 @@ class _PetHomePageState extends State<PetHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryWhiteColor,
-      appBar: AppBar(
-        leadingWidth: 70,
         backgroundColor: AppColors.primaryWhiteColor,
-        centerTitle: true,
-        leading: Padding(
-          padding: const EdgeInsets.only(left: 16.0),
-          child: GestureDetector(
-            onTap: () {
-              Navigator.pop(context);
-            },
-            child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(13))),
-                child: const Icon(
-                  Icons.arrow_back_ios_new,
-                  color: AppColors.primaryColor,
-                  size: 20,
-                )),
-          ),
-        ),
-        title: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-          decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.all(Radius.circular(30))),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: 15,
-                height: 15,
-                margin: const EdgeInsets.only(right: 15),
-                decoration: const BoxDecoration(
-                    color: Colors.lightGreenAccent, shape: BoxShape.circle),
+        appBar: AppBar(
+            leadingWidth: 70,
+            backgroundColor: AppColors.primaryWhiteColor,
+            centerTitle: true,
+            leading: Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: GestureDetector(
+                onTap: () {
+                  Navigator.pop(context);
+                },
+                child: Container(
+                    margin:
+                        const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.all(Radius.circular(13))),
+                    child: const Icon(
+                      Icons.arrow_back_ios_new,
+                      color: AppColors.primaryColor,
+                      size: 20,
+                    )),
               ),
-              const Text(
-                'Your pets are fine',
-                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
-              ),
-            ],
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.only(left: 16.0),
-            child: Container(
+            ),
+            title: Container(
                 padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
                 decoration: const BoxDecoration(
                     color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(13))),
-                child: const Icon(
-                  Icons.settings,
-                  color: AppColors.primaryColor,
-                  size: 20,
-                )),
-          ),
-          const SizedBox(width: 16)
-        ],
-      ),
-      body: const Column(
-        children: [PetCamViewWidget(), BottomButtonWidget()],
-      ),
-    );
+                    borderRadius: BorderRadius.all(Radius.circular(30))),
+                child: Row(mainAxisSize: MainAxisSize.min, children: [
+                  Container(
+                    width: 15,
+                    height: 15,
+                    margin: const EdgeInsets.only(right: 15),
+                    decoration: const BoxDecoration(
+                        color: Colors.lightGreenAccent, shape: BoxShape.circle),
+                  ),
+                  const Text('Your pets are fine',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.w400))
+                ])),
+            actions: [
+              Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 10),
+                      decoration: const BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.all(Radius.circular(13))),
+                      child: const Icon(Icons.settings,
+                          color: AppColors.primaryColor, size: 20))),
+              const SizedBox(width: 16)
+            ]),
+        body: const Column(
+          children: [PetCamViewWidget(), BottomButtonWidget()],
+        ));
   }
 }

e4f9a1b23a8565e987b84ee6d933ba52a019f115
ragulnathan <ragulsarma@gmail.com>
2024-03-17 22:43:17 +0530

    local assets and resources adding

--- lib/utils/app_colors.dart
+++ lib/utils/app_colors.dart
@@ -5,4 +5,5 @@ class AppColors {
   static const Color primaryColor = Color(0XFF181818);
   static const Color primaryBlueColor = Color(0XFF5285E8);
   static const Color primaryWhiteColor = Color(0XFFF0F5F4);
+  static const Color primaryButtonColor = Color(0XFFBFF85D);
 }

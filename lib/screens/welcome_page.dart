2ef2267cf6c5cf227d214b61ea592b4b191cd949
ragulnathan <ragulsarma@gmail.com>
2024-03-19 22:42:03 +0530

    alignemnt fix in common widgets

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,6 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:smart_home_ui/utils/app_colors.dart';
 import 'package:smart_home_ui/widgets/pet_page_widgets/bottom_button_widget.dart';
 import 'package:smart_home_ui/widgets/welcome_page_widgets/custom_circle.dart';
@@ -21,52 +19,48 @@ class _WelcomePageState extends State<WelcomePage> {
             // mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Expanded(
-                child: Stack(
-                  children: [
-                    Container(
-                      // height: 100,
-                      // width: 150,
-                      // padding: const EdgeInsets.only(left: 16.0, right: 40),
-                      // margin: const EdgeInsets.only(left: 16.0),
-                      decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                              colors: [
-                                // Color(0xFF2b2b33),
-                                // Color(0xFF2d3882),
-                                // Color(0xFF948b7b),
-                                Color(0xFF6f6a59),
-                                Color(0xFFddd6cc),
-                              ],
-                              begin: FractionalOffset(0.0, 1.0),
-                              end: FractionalOffset(1.0, 0.0),
-                              stops: [0.0, 1.0],
-                              tileMode: TileMode.clamp),
-                          borderRadius: BorderRadius.all(Radius.circular(20))),
-                    ),
-                    Container(
-                        // height: 350,
-                        // padding: const EdgeInsets.symmetric(
-                        //     horizontal: 20, vertical: 15),
-                        margin: const EdgeInsets.only(top: 35),
-                        decoration: const BoxDecoration(
-                            color: Colors.white,
-                            image: DecorationImage(
-                              image: AssetImage(
-                                  "assets/images/welcome_image.jpeg"),
-                              // fit: BoxFit.cover,
-                            ),
-                            shape: BoxShape.circle
-                            // borderRadius: BorderRadius.only(
-                            //     topLeft: Radius.circular(30),
-                            //     topRight: Radius.circular(30))
-                            )),
-                    /*CustomPaint(
+                  child: Stack(children: [
+                Container(
+                  // height: 100,
+                  // width: 150,
+                  // padding: const EdgeInsets.only(left: 16.0, right: 40),
+                  // margin: const EdgeInsets.only(left: 16.0),
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [
+                            // Color(0xFF2b2b33),
+                            // Color(0xFF2d3882),
+                            // Color(0xFF948b7b),
+                            Color(0xFF6f6a59),
+                            Color(0xFFddd6cc),
+                          ],
+                          begin: FractionalOffset(0.0, 1.0),
+                          end: FractionalOffset(1.0, 0.0),
+                          stops: [0.0, 1.0],
+                          tileMode: TileMode.clamp),
+                      borderRadius: BorderRadius.all(Radius.circular(20))),
+                ),
+                Container(
+                    // height: 350,
+                    // padding: const EdgeInsets.symmetric(
+                    //     horizontal: 20, vertical: 15),
+                    margin: const EdgeInsets.only(top: 35),
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        image: DecorationImage(
+                          image: AssetImage("assets/images/welcome_image.jpeg"),
+                          // fit: BoxFit.cover,
+                        ),
+                        shape: BoxShape.circle
+                        // borderRadius: BorderRadius.only(
+                        //     topLeft: Radius.circular(30),
+                        //     topRight: Radius.circular(30))
+                        )),
+                /*CustomPaint(
                       size: Size(200, 200),
                       painter: CirclePainter(),
                     ),*/
-                  ],
-                ),
-              ),
+              ])),
               Container(
                   // height: 250,
                   padding:
--- lib/widgets/welcome_page_widgets/custom_circle.dart
+++ lib/widgets/welcome_page_widgets/custom_circle.dart
@@ -4,15 +4,11 @@ class CirclePainter extends CustomPainter {
   final _paint = Paint()
     ..color = Colors.red
     ..strokeWidth = 2
-    // Use [PaintingStyle.fill] if you want the circle to be filled.
     ..style = PaintingStyle.stroke;
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawOval(
-      Rect.fromLTWH(0, 0, size.width, size.height),
-      _paint,
-    );
+    canvas.drawOval(Rect.fromLTWH(0, 0, size.width, size.height), _paint);
   }
 
   @override
